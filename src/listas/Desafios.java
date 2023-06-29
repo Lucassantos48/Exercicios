fa74263b3b4c8a433611acf12fd6d30c7ba8f3d4
Lucassantos48 <lucasmsantos049@gmail.com>
2023-06-28 21:56:19 -0300

    Create Desafios.java
    Arrays

---
+++ src/listas/Desafios.java
@@ -0,0 +1,17 @@
+package listas;
+
+import java.util.Arrays;
+
+public class Desafios {
+
+    public static void main(String[] args) {
+        
+        double[] notaAlunoA = new double[3];
+        notaAlunoA[0] = 5;
+        notaAlunoA[1] = 6;
+        notaAlunoA[2] = 8;
+
+        System.out.println(Arrays.toString(notaAlunoA));
+    }
+    
+}
